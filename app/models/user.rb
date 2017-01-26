d15ef40e5ec22ccb79e437e329c37315a60022a1
Rhuan Carlos <rhuancarlosg12@gmail.com>
2017-01-26 18:04:18 -0300

    Criando Controlle 'User' e 'Rooms'

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
-
+	
+	validates_presence_of :email, :full_name, :location, :password
+	validates_confirmation_of :password
+	validates_length_of :bio, minimum: 10, allow_blank: false
+	# validates :email, presence: true
+	# validates_presence_of :email
 end
