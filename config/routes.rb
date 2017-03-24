24a88714700407d5e1dfb36f08c7d5800c24a648
Rhuan Carlos <rhuancarlosg12@gmail.com>
2017-03-24 01:49:50 -0300

    Criado css p/ 'footer'; Modificado 'routes' p/ locate=ptBr; Concluida I18n p/ controler 'user'

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  resources :rooms
-  resources :users
+
+	scope "(:locale)", locale: /en|pt/ do
+	  resources :rooms
+	  resources :users
+	end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # Custom Routes
+  get '/:locate' => 'home#index', locale: /en|pt/
   root 'home#index' # Mudando a pagina inicial do rails para o controlle 'home' action 'index'.
 
 end
